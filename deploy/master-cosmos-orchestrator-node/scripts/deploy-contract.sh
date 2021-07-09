ecb2f100fcf8450564e8df4b7157858bf3896007
sunny960 <sunnypardhan80@gmail.com>
2021-07-09 20:22:53 +0530

    add peer node script 3444556

--- deploy/master-cosmos-orchestrator-node/scripts/deploy-contract.sh
+++ deploy/master-cosmos-orchestrator-node/scripts/deploy-contract.sh
@@ -53,7 +53,7 @@ npx ts-node \
     --eth-node="http://$ETH_HOST:8545" \
     --eth-privkey="$ETH_MINER_PRIVATE_KEY" \
     --contract=artifacts/contracts/Gravity.sol/Gravity.json \
-    --test-mode=true | grep "Gravity deployed at Address" | grep -Eow '0x[0-9a-fA-F]{40}' >> $GRAVITY_HOME/eth_contract_address
+    --test-mode=true | grep "Gravity deployed at Address" | grep -Eow '0x[0-9a-fA-F]{40}' >> /root/testchain/gravity/eth_contract_address
 
 CONTRACT_ADDRESS=$(cat $GRAVITY_HOME/eth_contract_address)
 echo "Contract address: $CONTRACT_ADDRESS"
