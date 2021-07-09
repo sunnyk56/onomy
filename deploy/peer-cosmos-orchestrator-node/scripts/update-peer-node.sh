c6d4c31493ae3ae553c71af1fd04320fd58c6059
sunny960 <sunnypardhan80@gmail.com>
2021-07-09 14:35:55 +0530

    add peer node script 344

--- deploy/peer-cosmos-orchestrator-node/scripts/update-peer-node.sh
+++ deploy/peer-cosmos-orchestrator-node/scripts/update-peer-node.sh
@@ -1,8 +1,9 @@
-ROOT="/root"
 GRAVITY_GENESIS_FILE="/root/testchain/gravity/config/genesis.json"
 GRAVITY_ASSETS="/root/assets"
 BUCKET_MASTER_GENESIS_FILE="master/genesis.json"
 BUCKET_MASTER="/root/onomy/master"
+
+GRAVITY_HOME_FLAG="--home /root/testchain/gravity"
 
 GIT_HUB_BRANCH=$1
 
@@ -20,7 +21,8 @@ rm -f $GRAVITY_GENESIS_FILE
 touch $GRAVITY_GENESIS_FILE
 cp -r $BUCKET_MASTER/assets/. $GRAVITY_ASSETS
 
-cd $ROOT
+echo "Run the gravity start scripts"
+gravity $GRAVITY_HOME_FLAG start --pruning=nothing &>/dev/null
 
-echo "Run the cosmos-run scripts"
-sh /root/scripts/cosmos-run.sh
+#echo "Run the cosmos-run scripts"
+#sh /root/scripts/cosmos-run.sh
