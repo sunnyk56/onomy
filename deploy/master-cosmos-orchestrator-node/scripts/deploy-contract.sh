1d00c1056a321f15387caa31d0f44fc3ea526dd6
sunny960 <sunnypardhan80@gmail.com>
2021-07-09 17:54:46 +0530

    add peer node script 3444556

--- deploy/master-cosmos-orchestrator-node/scripts/deploy-contract.sh
+++ deploy/master-cosmos-orchestrator-node/scripts/deploy-contract.sh
@@ -12,12 +12,12 @@ GRAVITY=gravity
 # The name of the gravity node
 GRAVITY_NODE_NAME="gravity"
 # The address to run gravity node
-#GRAVITY_HOST="0.0.0.0"
+GRAVITY_HOST="143.244.147.226"
 GIT_HUB_USER=$1
 GIT_HUB_PASS=$2
 GIT_HUB_EMAIL=$3
 GIT_HUB_BRANCH=$4
-GRAVITY_HOST=$5
+#GRAVITY_HOST=$5
 # Home folder for gravity config
 GRAVITY_HOME="$CURRENT_WORKING_DIR/$CHAINID/$GRAVITY_NODE_NAME"
 # Home flag for home folder
@@ -30,8 +30,8 @@ STAKE_DENOM="stake"
 ETH_MINER_PRIVATE_KEY="0xb1bab011e03a9862664706fc3bbaa1b16651528e5f0e7fbfcbfdd8be302a13e7"
 ETH_MINER_PUBLIC_KEY="0xBf660843528035a5A4921534E156a27e64B231fE"
 # The host of ethereum node
-#ETH_HOST="0.0.0.0"
-ETH_HOST=$6
+ETH_HOST="143.244.147.226"
+#ETH_HOST=$6
 
 
 echo "Applying contracts"
@@ -39,10 +39,18 @@ GRAVITY_DIR=/go/src/github.com/onomyprotocol/gravity-bridge/
 cd $GRAVITY_DIR/solidity
 ls
 echo "running contract-deployer.ts: GRAVITY_HOST: $GRAVITY_HOST ETH_HOST: $ETH_HOST"
+echo ts-node \
+    contract-deployer.ts \
+    --cosmos-node="http://$GRAVITY_HOST:26657" \
+    --eth-node="http://$ETH_HOST:8545" \
+    --eth-privkey="$ETH_MINER_PRIVATE_KEY" \
+    --contract=artifacts/contracts/Gravity.sol/Gravity.json \
+    --test-mode=true
+
 npx ts-node \
     contract-deployer.ts \
-    --cosmos-node="http://'$GRAVITY_HOST':26657" \
-    --eth-node="http://'$ETH_HOST':8545" \
+    --cosmos-node="http://$GRAVITY_HOST:26657" \
+    --eth-node="http://$ETH_HOST:8545" \
     --eth-privkey="$ETH_MINER_PRIVATE_KEY" \
     --contract=artifacts/contracts/Gravity.sol/Gravity.json \
     --test-mode=true | grep "Gravity deployed at Address" | grep -Eow '0x[0-9a-fA-F]{40}' >> $GRAVITY_HOME/eth_contract_address
