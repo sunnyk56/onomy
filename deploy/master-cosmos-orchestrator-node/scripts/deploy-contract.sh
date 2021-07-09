f761529bb1103aee9116127950ba20b06d6e09d0
sunny960 <sunnypardhan80@gmail.com>
2021-07-09 19:44:44 +0530

    add peer node script 3444556

--- deploy/master-cosmos-orchestrator-node/scripts/deploy-contract.sh
+++ deploy/master-cosmos-orchestrator-node/scripts/deploy-contract.sh
@@ -63,5 +63,5 @@ echo "Contract address: $CONTRACT_ADDRESS"
 #sh deploy/master-cosmos-orchestrator-node/scripts/store-ethereum-contract-info.sh $GIT_HUB_USER $GIT_HUB_PASS $GIT_HUB_EMAIL $GIT_HUB_BRANCH
 
 # return back to home
-#cd $CURRENT_WORKING_DIR
+cd $CURRENT_WORKING_DIR
 #sleep 10
