2a33fad8e02029a2faa445daa3b55c615cd305e6
sunny960 <sunnypardhan80@gmail.com>
2021-07-12 17:25:52 +0530

    add peer node script 34445563445

--- deploy/master-cosmos-orchestrator-node/scripts/run.sh
+++ deploy/master-cosmos-orchestrator-node/scripts/run.sh
@@ -22,7 +22,9 @@ GRAVITY_ADDRESS_PREFIX=cosmos
 # Gravity chain demons
 STAKE_DENOM="stake"
 
+#ETH_MINER_PRIVATE_KEY="0xb1bab011e03a9862664706fc3bbaa1b16651528e5f0e7fbfcbfdd8be302a13e7"
 #ETH_MINER_PUBLIC_KEY="0xBf660843528035a5A4921534E156a27e64B231fE"
+ETH_MINER_PRIVATE_KEY="c80c8a1a8da0bb6cf08dc95dd9e70682bac967ec341557e2b946b0a670c291dd"
 ETH_MINER_PUBLIC_KEY="0x90Dba8eD66c33508285Bc5ADB411356b577079A2"
 # The host of ethereum node
 ETH_HOST="0.0.0.0"
@@ -33,6 +35,7 @@ echo "Starting $GRAVITY_NODE_NAME"
 $GRAVITY $GRAVITY_HOME_FLAG start --pruning=nothing &>/dev/null
 sleep 10
 #-------------------- Run ethereum (geth) --------------------
+echo "Running ethereum node"
 
 geth --identity "GravityTestnet" \
     --nodiscover \
@@ -52,10 +55,21 @@ geth --identity "GravityTestnet" --nodiscover \
                                --miner.etherbase="$ETH_MINER_PUBLIC_KEY" \
                                &>/dev/null
 sleep 10
-#-------------------- Run orchestrator --------------------
+
+echo "deploying contract"
+
+npx ts-node \
+    contract-deployer.ts \
+    --cosmos-node="http://$GRAVITY_HOST:26657" \
+    --eth-node="http://$ETH_HOST:8545" \
+    --eth-privkey="$ETH_MINER_PRIVATE_KEY" \
+    --contract=artifacts/contracts/Gravity.sol/Gravity.json \
+    --test-mode=true | grep "Gravity deployed at Address" | grep -Eow '0x[0-9a-fA-F]{40}' >> /root/testchain/gravity/eth_contract_address
 
 CONTRACT_ADDRESS=$(cat $GRAVITY_HOME/eth_contract_address)
 echo "Contract address: $CONTRACT_ADDRESS"
+#-------------------- Run orchestrator --------------------
+
 
 echo "Gathering keys for orchestrator"
 COSMOS_GRPC="http://$GRAVITY_HOST:9090/"
