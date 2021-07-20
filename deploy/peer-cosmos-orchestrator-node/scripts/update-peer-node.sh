d47f2970d24d495fb45730067030d08cf62954f9
sunny960 <sunnypardhan80@gmail.com>
2021-07-20 12:21:58 +0530

    update peer node script

--- deploy/peer-cosmos-orchestrator-node/scripts/update-peer-node.sh
+++ deploy/peer-cosmos-orchestrator-node/scripts/update-peer-node.sh
@@ -1,61 +1,14 @@
-# GRAVITY_GENESIS_FILE="/root/testchain/gravity/config/genesis.json"
 GRAVITY_ASSETS="/root/assets"
-# BUCKET_MASTER_GENESIS_FILE="master/genesis.json"
 BUCKET_MASTER="/root/onomy/master"
 
-# GIT_HUB_BRANCH=$1
-
-# echo "Get pull updates"
-# git pull origin $GIT_HUB_BRANCH
-
-# echo "updating EthGenesis.json in the root assets directory"
-# cp $BUCKET_MASTER/. $GRAVITY_ASSETS
-
-
-# echo "Copying genesis file"
-# rm -f $GRAVITY_GENESIS_FILE
-# touch $GRAVITY_GENESIS_FILE
-# cp $BUCKET_MASTER_GENESIS_FILE $GRAVITY_GENESIS_FILE
-
-# echo "Run the cosmos-run scripts"
-# sh /root/scripts/cosmos-run.sh# GRAVITY_GENESIS_FILE="/root/testchain/gravity/config/genesis.json"
-# GRAVITY_ASSETS="/root/assets"
-# BUCKET_MASTER_GENESIS_FILE="master/genesis.json"
-# BUCKET_MASTER="/root/onomy/master"
-
 GRAVITY_HOME_FLAG="--home /root/testchain/gravity"
-
-# GIT_HUB_BRANCH=$1
-
-# echo "Get pull updates"
-# git pull origin $GIT_HUB_BRANCH
-
-# echo "updating EthGenesis.json in the root assets directory"
-# rm -rf $GRAVITY_ASSETS
-# mkdir $GRAVITY_ASSETS
-# cp $BUCKET_MASTER/assets/. $GRAVITY_ASSETS
-
-
-# echo "Copying genesis file"
-# rm -f $GRAVITY_GENESIS_FILE
-# touch $GRAVITY_GENESIS_FILE
-# cp -r $BUCKET_MASTER/assets/. $GRAVITY_ASSETS
-
-# echo "Run the gravity start scripts"
-# gravity $GRAVITY_HOME_FLAG start --pruning=nothing &>/dev/null
-
-#echo "Run the cosmos-run scripts"
-#sh /root/scripts/cosmos-run.sh
 GRAVITY_GENESIS_FILE="/root/testchain/gravity/config"
 GRAVITY_CONFIG_FILE="/root/testchain/gravity/config/config.toml"
 BUCKET_MASTER_GENESIS_FILE="/root/mainNode/master/genesis.json"
 BUCKET_MASTER_SEED_FILE="/root/mainNode/master/seed"
 MAIN_NODE="/root/mainNode"
 
-GIT_HUB_USER=$1
-GIT_HUB_PASS=$2
-GIT_HUB_EMAIL=$3
-GIT_HUB_BRANCH=$4
+GIT_HUB_BRANCH=$1
 
 echo "Get pull updates"
 git clone -b $GIT_HUB_BRANCH https://github.com/sunnyk56/onomy.git $MAIN_NODE
@@ -67,7 +20,8 @@ cp $BUCKET_MASTER/assets/. $GRAVITY_ASSETS
 
 echo "Copying genesis file"
 rm $GRAVITY_GENESIS_FILE/genesis.json
-cp $BUCKET_MASTER_GENESIS_FILE  $GRAVITY_GENESIS_FILE
+touch $GRAVITY_GENESIS_FILE/genesis.json
+cp $BUCKET_MASTER_GENESIS_FILE $GRAVITY_GENESIS_FILE/genesis.json
 peerseed=$(cat $BUCKET_MASTER_SEED_FILE)
 
 sed -i 's#persistent_peers = ""#persistent_peers = "'$peerseed'"#g' $GRAVITY_CONFIG_FILE
